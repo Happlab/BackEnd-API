9c959cd48d78ebaff724b24615700ed391d8e2f2
DaaC <andrescd@unicauca.edu.co>
2022-06-21 15:13:42 -0500

    CORS Fix Local host
    se concedieron permisos CORS a localhost:8080

--- src/main/java/co/edu/unicauca/APIHappLab/controller/rate_controller.java
+++ src/main/java/co/edu/unicauca/APIHappLab/controller/rate_controller.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import co.edu.unicauca.APIHappLab.service.rate_service;
 
 @RestController
 @RequestMapping("/rate")
+@CrossOrigin(origins= "http://localhost:8080")
 public class rate_controller {
 	
 	@Autowired
--- src/main/java/co/edu/unicauca/APIHappLab/controller/usuario_controller.java
+++ src/main/java/co/edu/unicauca/APIHappLab/controller/usuario_controller.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import co.edu.unicauca.APIHappLab.service.persona_service;
 
 @RestController
 @RequestMapping("/persona")
+@CrossOrigin(origins= "http://localhost:8080")
 public class usuario_controller {
 	@Autowired
 	private persona_service service;
