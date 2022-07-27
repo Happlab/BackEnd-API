2ec00fad0e205d231445ec79d4a76d7d066de640
DaaC <andrescd@unicauca.edu.co>
2022-07-27 18:08:25 -0500

    eliminacion de imports
    se eliminaron import innecesarios y comentarios en desuso

--- src/main/java/co/edu/unicauca/APIHappLab/controller/noticia_controller.java
+++ src/main/java/co/edu/unicauca/APIHappLab/controller/noticia_controller.java
@@ -17,7 +17,6 @@ import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
