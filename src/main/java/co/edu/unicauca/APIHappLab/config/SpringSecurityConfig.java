5030c74172328f7944dab64b38ec3497b19b909e
DaaC <andrescd@unicauca.edu.co>
2022-07-28 17:34:54 -0500

    quitado token de trespuesta
    se quito la string "token que se enviaba con el token"

--- src/main/java/co/edu/unicauca/APIHappLab/config/SpringSecurityConfig.java
+++ src/main/java/co/edu/unicauca/APIHappLab/config/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package co.edu.unicauca.APIHappLab.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,11 +16,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import co.edu.unicauca.APIHappLab.config.filter.JwtRequestFilter;
+import co.edu.unicauca.APIHappLab.enums.Role;
 import co.edu.unicauca.APIHappLab.service.persona_service;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter implements ApplicationContextAware{
     @Autowired
 	private persona_service usuarioService;
     
@@ -44,11 +46,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+		http
+		.csrf().disable()
 		.authorizeRequests()
-	//	.antMatchers("/**").permitAll()
-		.antMatchers(HttpMethod.GET, "/persona/Login/**","/persona/auth/**", "/persona/registro").permitAll()
-		.antMatchers(HttpMethod.GET, "/persona/", "/persona/{Email}").hasRole("ADMIN")
+		.antMatchers("/**").permitAll()
+		.antMatchers(HttpMethod.GET, "/persona/auth/**", "/persona/registro").permitAll()
+		.antMatchers(HttpMethod.GET, "/persona/", "/persona/{Email}").hasRole(Role.ADMIN.name())
 		.antMatchers(HttpMethod.PUT, "/persona/update").hasAnyRole("USER", "ADMIN")
 		.antMatchers(HttpMethod.DELETE, "/persona/desactivar/{Email}", "/persona/delete/{Email}").hasRole("ADMIN")
 		.antMatchers("/persona/**").hasRole("ADMIN")
@@ -58,6 +61,4 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 	http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}
-
-
 }
